1e54a86be39dd53e44b6404fa7023e2944a88ff9
saishkulkarni <saishkulkarni7@gmail.com>
2023-08-17 13:50:04 +0530

    First Time

---
+++ src/main/java/controller/EditTask.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDao;
+
+@WebServlet("/edittask")
+public class EditTask extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if (req.getSession().getAttribute("user") == null) {
+			resp.getWriter().print("<h1 style='color:red'>Session Expired, Login Again</h1>");
+			req.getRequestDispatcher("Login.html").include(req, resp);
+		} else {
+			int id = Integer.parseInt(req.getParameter("id"));
+			UserDao dao = new UserDao();
+			req.setAttribute("task", dao.fetchTask(id));
+			req.getRequestDispatcher("EditTask.jsp").forward(req, resp);
+		}
+	}
+}
